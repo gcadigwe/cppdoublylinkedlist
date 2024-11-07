063fedf3401a30df5edbfb6ce26907d26ce5de0c
gcadigwe@gmail.com <gcadigwe@gmail.com>
2024-11-07 17:35:56 +0100

    doubly linked list, insertion, backward and forward traversal

--- doubly-linked-list/main.cpp
+++ doubly-linked-list/main.cpp
@@ -7,8 +7,67 @@
 
 #include <iostream>
 
+struct Node {
+    Node* prev;
+    int data;
+    Node* next;
+    
+    Node(int value) : data(value), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+private:
+    Node* head;
+    Node* tail;
+    
+public:
+    DoublyLinkedList():head(nullptr), tail(nullptr){}
+    
+    void append(int data) {
+        
+        Node* newNode = new Node(data);
+        
+        if(!head){
+            head = tail = newNode;
+        }else{
+            tail -> next = newNode;
+            newNode -> prev = tail;
+            tail = newNode;
+        }
+    }
+    
+    void displayForward() {
+        Node* current = head;
+        
+        while(current) {
+            std::cout << current -> data << " -> ";
+            current = current -> next;
+        }
+        std::cout << "null" << std::endl;
+    }
+    
+    void displayBackward() {
+        Node* current = tail;
+        
+        while(current) {
+            
+            std::cout << current -> data << " -> ";
+            current = current -> prev;
+        }
+        
+        std::cout << "null" << std::endl;
+    }
+    
+};
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    DoublyLinkedList list;
+    list.append(10);
+    list.append(20);
+    list.append(30);
+    
+    list.displayForward();
+    list.displayBackward();
     return 0;
 }
