4ef3d6ba710111e55746b8d0d891bad0141177bb
gcadigwe@gmail.com <gcadigwe@gmail.com>
2024-11-07 15:58:20 +0100

    Initial Commit

---
+++ doubly-linked-list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  doubly-linked-list
+//
+//  Created by Godswill Adigwe on 07/11/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
